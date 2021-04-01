847e7bcc17c2d62c421f81c50f5de454b6ba6286
Joshua Wang <joshua@outlook.com>
2021-04-01 19:23:31 +0800

    wpf

---
+++ Controls/DataGrid/1.DataGrid/Readme.md
@@ -0,0 +1,4 @@
+Lesson 1 - Add a GridControl to a Project:
+https://docs.devexpress.com/WPF/96441/controls-and-libraries/data-grid/getting-started/NET-Core/lesson-1-add-a-gridcontrol-to-a-project
+
+不使用northwind数据库，最简单的演示
---
+++ Devexpress/Common/Layout/Layout/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core.Serialization;
+
+namespace Layout
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public string LayoutPath = "Layout.xml";
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			//Save the layout of serializable DevExpress controls starting with the current object (specified by "this").
+			//If the current object ("this") is also a serializable DevExpress control, its layout is saved as well.
+			DXSerializer.Serialize(this, LayoutPath, "app", null);
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (File.Exists(LayoutPath))
+				DXSerializer.Deserialize(this, LayoutPath, "app", null);
+		}
+	}
+}
