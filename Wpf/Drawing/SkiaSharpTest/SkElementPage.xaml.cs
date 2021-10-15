18e1489dbb7651b26845618989c3b5be266b4904
yacper <yacper@gmail.com>
2021-10-15 18:48:38 +0800

    skiasharp 测试

---
+++ Wpf/Drawing/SkiaSharpTest/SkElementPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+
+namespace SkiaSharpTest
+{
+    /// <summary>
+    /// SkElementPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class SkElementPage : Window
+    {
+        public SkElementPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            // the the canvas and properties
+            var canvas = e.Surface.Canvas;
+
+            // make sure the canvas is blank
+            canvas.Clear(SKColors.White);
+
+			// draw some text
+			var paint = new SKPaint(new SKFont(SKTypeface.FromFamilyName("Microsoft YaHei UI")))
+			{
+				Color = SKColors.Black,
+				IsAntialias = true,
+				Style = SKPaintStyle.Fill,
+				TextAlign = SKTextAlign.Center,
+				TextSize =12 
+			};
+			var coord = new SKPoint(e.Info.Width / 2, (e.Info.Height + paint.TextSize) / 2);
+			canvas.DrawText("SkiaSharp", coord, paint);
+		}
+
+    }
+}
