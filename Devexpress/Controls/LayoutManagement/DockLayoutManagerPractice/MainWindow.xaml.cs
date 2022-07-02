b3922ab68f629062040a042926d80216a0f9461e
yacper <yacper@gmail.com>
2022-07-02 23:00:06 +0800

    。

---
+++ Devexpress/Controls/LayoutManagement/DockLayoutManagerPractice/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Core.Serialization;
+using DevExpress.Xpf.Docking;
+using DevExpress.Xpf.Layout.Core;
+using DockLayoutManagerPractice.Views;
+
+namespace DockLayoutManagerPractice
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow:Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+
+			if(File.Exists("test.xml"))
+			{
+				//WorkspaceManager workspaceManager = WorkspaceManager.GetWorkspaceManager(docklayoutmanager1) as WorkspaceManager;
+				//workspaceManager.LoadWorkspace("workspace1", "test.xml");
+				//workspaceManager.ApplyWorkspace("workspace1");
+
+				docklayoutmanager1.DockItemRestoring += (a, b) =>
+				{
+
+				};
+				docklayoutmanager1.DockItemRestored += (a, b) =>
+				{
+
+				};
+
+
+
+				docklayoutmanager1.LayoutItemRestored += (a, b) =>
+				{
+
+
+				};
+
+
+				// 1. restore 只会还原layout，不会创建其中的content
+				docklayoutmanager1.RestoreLayoutFromXml("test.xml");
+
+				//foreach (var item in docklayoutmanager1.Items)
+				//{
+				//	Debug.WriteLine(item);
+					
+				//}
+
+				foreach (var item in docklayoutmanager1.LayoutRoot.Items)
+				{
+					Debug.WriteLine(item);
+					
+				}
+
+
+				var containerList  = DevExpress.Mvvm.UI.LayoutTreeHelper.GetVisualChildren(this).OfType<LayoutPanel>();
+
+				// 2. 当layout还原的时候，可以layoutpanel的tag的属性，还原layoutPanel的Content控件
+				var containerList1 = DevExpress.Mvvm.UI.LayoutTreeHelper.GetLogicalChildren(this).OfType<LayoutPanel>();
+				foreach (LayoutPanel item in containerList1)
+				{
+					Debug.WriteLine(item);
+					Debug.WriteLine($"Panel Name:{item.Name} Tag:{item.Tag}");
+					if(item.Name == "ErrorList")
+						(item as LayoutPanel).Content = new ErrorList();
+				}
+
+
+				return;
+				// 这种也可以
+				foreach (FloatGroup fg in docklayoutmanager1.FloatGroups)
+				{
+					Debug.WriteLine(fg);
+					foreach (BaseLayoutItem item in fg.Items)
+					{
+						Debug.WriteLine(item);
+						Debug.WriteLine($"Panel Name:{item.Name} Tag:{item.Tag}");
+						if (item.Name == "ErrorList")
+							(item as LayoutPanel).Content = new ErrorList();
+					}
+				}
+
+
+				//var containerList = DevExpress.Mvvm.UI.LayoutTreeHelper.GetVisualChildren(docklayoutmanager1).OfType<LayoutPanel>();
+				//var docList = DevExpress.Mvvm.UI.LayoutTreeHelper.GetLogicalChildren(docklayoutmanager1).OfType<LayoutPanel>();
+
+				//foreach (var VARIABLE in docklayoutmanager1.LayoutRoot.chi)
+				//{
+					
+				//}
+
+			}
+			else
+			{
+
+
+				//LayoutControlItem lc    = new LayoutControlItem();
+				//lc.Content = new ErrorList();
+				var               panel = docklayoutmanager1.DockController.AddPanel(DockType.None);
+				panel.Caption = "ErrorList";
+				panel.Name    = "ErrorList";
+				panel.Tag     = "ErrorList 1:abc";
+				panel.Content = new ErrorList();
+				//DXSerializer.SetSerializationID(panel.Content as DependencyObject, "ErrorList");
+				//var               panel2 = docklayoutmanager1.DockController.AddPanel(DockType.None);
+				//panel2.Caption = "ErrorList";
+				//panel2.Name = "ErrorList2";
+				//panel2.Content = new ErrorList();
+				//DXSerializer.SetSerializationID(panel2.Content as DependencyObject, "ErrorList2");
+	
+				FloatGroup floatgroup = new FloatGroup();
+				floatgroup.Name = "TestFloatGroup";
+				floatgroup.FloatSize = new Size(500, 500);
+				floatgroup.FloatLocation = new Point(300, 300);
+				floatgroup.Add(panel);
+				//floatgroup.Add(panel2);
+				docklayoutmanager1.FloatGroups.Add(floatgroup);
+				docklayoutmanager1.BeginUpdate();
+
+			}
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+
+			string xmlFilePath = System.AppDomain.CurrentDomain.BaseDirectory + "test.xml";
+
+				//WorkspaceManager workspaceManager = WorkspaceManager.GetWorkspaceManager(docklayoutmanager1) as WorkspaceManager;
+				//workspaceManager.CaptureWorkspace("workspace1");
+				//workspaceManager.SaveWorkspace("workspace1", "test.xml");
+
+
+				//return;
+
+			if(File.Exists("test.xml"))
+			{
+				docklayoutmanager1.SaveLayoutToXml("test.xml");
+
+			}
+			else
+			{
+				using(File.Create("test.xml")) { }
+				docklayoutmanager1.SaveLayoutToXml("test.xml");
+
+		}
+		}
+	}
+}
--- Wpf/Drawing/SkiaSharpSample/Tutorial/Tutorial_1_Hello.md
+++ Wpf/Drawing/SkiaSharpSample/Tutorial/Tutorial_1_Hello.md
@@ -2,13 +2,47 @@
 
 SkiaSharp 是一个适用于 .NET 和 C# 的 2D 图形系统，由开源 Skia 图形引擎支持，在 Google 产品中广泛使用。
 
-## 引用库
+## 安装
+
+
+
+## 基本概念
+### 屏幕坐标系
+
+屏幕的坐标系原点在屏幕的左上角，水平往右、竖直往下为正。屏幕的坐标横坐标用“x”表示，纵坐标用“y”表示，坐标的单位为像素。坐标（4， 2）用表示当前点在原点右方4个像素处，在原点下方2个像素处，
 
 ![skiasharp coordinate](Images/coordinate.jpg "skiasharp coordinate")
 
+### 颜色 skcolor
+颜色的构造方法有很多种，但最常见的是利用rgb三原色来构造，此外还可以加入透明度：
 
+```c#
+var color = new skcolor(180, 180, 180, 128);    //四个参数表示red, green, blue, alpha 
+```
 
-
+### 画刷 skpaint
+skia是用画刷来完成各种绘制工作的。画刷的构造函数并无参数输入，各种参数是以属性的方式传入的。常见的属性有：颜色、字体、填充类型、画笔宽度等。
+```C#
+var paint = new SKPaint()
+{
+    Color = new SKColor(180, 180, 180, 128), //颜色
+    StrokeWidth = 2, //画笔宽度
+    Typeface = SKTypeface.FromFamilyName("宋体", SKFontStyle.Normal), //字体
+    TextSize = 32,  //字体大小
+    Style = SKPaintStyle.Stroke, //类型：填充 或 画边界 或全部
+    PathEffect = SKPathEffect.CreateDash(LongDash, 0),   //绘制虚线
+};
+```
+### 画布 SKCanvas
+Skia所有的绘制是基于画布的。画布来自于SKSurface，SKSurface一般从图像从获取。画布绘制通常直接调用其DrawXXX方法，其函数意义及所需参数大都可通过其名称轻易判断。而本项目中海图直接显示窗体中的SKControl控件上，该控件的的PaintSurface事件中存在画布。
+```C#
+SKImageInfo imageInfo = new SKImageInfo(300, 250);
+using (SKSurface surface = SKSurface.Create(imageInfo))
+{
+    SKCanvas canvas = surface.Canvas;
+    canvas.DrawColor(SKColors.Red);  //填充颜色
+}
+```
 
 参考:
 
