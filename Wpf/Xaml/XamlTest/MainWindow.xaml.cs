39c413220e5142a9474c18c081c30f75ecd1e70e
yacper <yacper@gmail.com>
2022-10-24 00:26:27 +0800

    xamltest

---
+++ Wpf/Xaml/XamlTest/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XamlTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Window window = null;
+            //using (FileStream fs = new FileStream("MyWindow.xaml", FileMode.Open, FileAccess.Read))
+            //{
+            //    // Get the root element, which we know is a Window
+            //    window = (Window)XamlReader.Load(fs);
+            //}
+        }
+    }
+}
