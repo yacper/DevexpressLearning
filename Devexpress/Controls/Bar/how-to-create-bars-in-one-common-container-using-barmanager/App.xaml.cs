7751f808d24effe735f4dee450feec48d25d4020
yacper <yacper@gmail.com>
2022-11-01 18:30:53 +0800

    commandvm

---
+++ Devexpress/Controls/Bar/how-to-create-bars-in-one-common-container-using-barmanager/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace how_to_create_bars_in_one_common_container_using_barmanager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
--- Devexpress/Controls/Neo/NeoControls/View/SiginTools.xaml.cs
+++ Devexpress/Controls/Neo/NeoControls/View/SiginTools.xaml.cs
@@ -19,56 +19,127 @@ using System.Windows.Shapes;
 
 namespace NeoControls
 {
-    public enum ConnectedStatus
+public enum ConnectedStatus
+{
+    Connected,
+    Disconnected,
+}
+
+public class Provider : BindableBase
+{
+    public override string ToString() => Name;
+
+    public         string          Name   { get;                              set; } = "P1";
+    public         ConnectedStatus Status { get;                              set; } = ConnectedStatus.Disconnected;
+    public virtual ImageSource     Source { get => GetProperty(() => Source); set => SetProperty(() => Source, value); }
+
+    public void Toggle()
     {
-        Connected,
-        Disconnected,
+        Status = Status == ConnectedStatus.Disconnected ? ConnectedStatus.Connected : ConnectedStatus.Disconnected;
+        Source = Status == ConnectedStatus.Disconnected ? null : Images.ConnectedStatus;
+    }
+}
+
+/// <summary>
+/// SiginTools.xaml 的交互逻辑
+/// </summary>
+public partial class SiginTools : UserControl
+{
+    public ObservableCollection<CommandVm> PVms { get; set; }
+
+    public SiginTools()
+    {
+        InitializeComponent();
+        this.DataContext = this;
+        InitData();
     }
 
-    public class Privoider: BindableBase
+    private void InitData()
     {
-        public ConnectedStatus Status { get; set; } = ConnectedStatus.Disconnected;
-        public virtual ImageSource Source { get => GetProperty(() => Source); set => SetProperty(() => Source, value); }
-
-        public void Toggle()
-        {
-            Status = Status == ConnectedStatus.Disconnected ? ConnectedStatus.Connected : ConnectedStatus.Disconnected;
-            Source = Status == ConnectedStatus.Disconnected ? null : Images.ConnectedStatus;
-        }
-
-        
-    }
-    /// <summary>
-    /// SiginTools.xaml 的交互逻辑
-    /// </summary>
-    public partial class SiginTools : UserControl
-    {
-        public Privoider Privoider { get; set; }
-        public ObservableCollection<CommandVm> PVms { get; set; }
-        public SiginTools()
-        {
-            InitializeComponent();
-            this.DataContext = this;
-            InitData();
-        }
-
-        private void InitData()
-        {
-            Privoider = new Privoider();
-            PVms = new ObservableCollection<CommandVm>() 
+        var vm = new CommandVm()
             {
-                new CommandVm()
-                {
-                    DisplayName = "TCP_Privorder",
-                    DisplayMode = BarItemDisplayMode.ContentAndGlyph,
-                    Glyph = Images.Monitor,
-                    StateImg = Images.ConnectedStatus,
-                    Command = new DelegateCommand(() =>
-                    {
-                        Privoider.Toggle();
-                    })
-                }.WithPropertyBinding<Privoider>(T=>T.StateImg, Privoider, S=>S.Source),
-            };
-        }
+                Owner = new Provider() { Name = "P1" },
+                //DisplayName = ,
+                //DisplayNameExpression = (vm) =>((vm.Owner as Provider).Name),
+                DisplayMode = BarItemDisplayMode.ContentAndGlyph,
+                Glyph       = Images.Monitor,
+                //StateImg    = Images.ConnectedStatus,
+                Command = new DelegateCommand<FrameworkContentElement>((e) =>
+                                                                       {
+                                                                           Console.WriteLine(e.ToString());
+                                                                           ((e.DataContext as CommandVm).Owner as Provider).Toggle();
+                                                                       }
+                                                                      )
+            }
+        .WithPropertyBinding(T => T.StateImg, S => (S.Owner as Provider).Source)
+             .WithPropertyBinding(T => T.DisplayName, S => (S.Owner as Provider).Name);
+
+            CommandVm vm2 = vm.Clone(new Provider() { Name = "p2" })
+                    ;
+            //vm2.DisplayName = "p2";
+
+
+            //var vm2 = new CommandVm()
+            //      {
+            //          Owner = new Provider() { Name = "P2" },
+            //          //DisplayName = ,
+            //          //DisplayNameExpression = (vm) =>((vm.Owner as Provider).Name),
+            //          DisplayMode = BarItemDisplayMode.ContentAndGlyph,
+            //          Glyph       = Images.Monitor,
+            //          //StateImg    = Images.ConnectedStatus,
+            //          Command = new DelegateCommand<FrameworkContentElement>((e) =>
+            //                                                                 {
+            //                                                                     Console.WriteLine(e.ToString());
+            //                                                                     ((e.DataContext as CommandVm).Owner as Provider).Toggle();
+            //                                                                 }
+            //                                                                )
+            //      }
+            //  //.WithPropertyBinding(T => T.StateImg, S => (S.Owner as Provider).Source)
+            //       .WithPropertyBinding(T => T.DisplayName, S => (S.Owner as Provider).Name);
+
+
+            ;
+
+            //vm2.Command.Execute(null);
+
+
+            PVms = new ObservableCollection<CommandVm>()
+        {
+            vm,
+            vm2
+        };
+
+
+        //PVms = new ObservableCollection<CommandVm>()
+        //{
+        //    new CommandVm()
+        //    {
+        //        Owner       = new Provider(){Name = "P1"},
+        //        //DisplayName = ,
+        //        //DisplayNameExpression = (vm) =>((vm.Owner as Provider).Name),
+        //        DisplayMode           = BarItemDisplayMode.ContentAndGlyph,
+        //        Glyph                 = Images.Monitor,
+        //        //StateImg    = Images.ConnectedStatus,
+        //        Command = new DelegateCommand<FrameworkContentElement>((e) =>
+        //                                                 {
+
+        //                                                     Console.WriteLine(e.ToString());
+        //                                                     ((e.DataContext as CommandVm).Owner as Provider).Toggle();
+        //                                                 }
+        //                                                )
+        //    }.WithPropertyBinding(T => T.StateImg, S => (S.Owner as Provider).Source)
+        //    .WithPropertyBinding(T => T.DisplayName, S => (S.Owner as Provider).Name)
+
+        //    //.WithCommand(new DelegateCommand(() =>
+        //    //                                             {
+
+        //    //                                                 //Console.WriteLine(e.ToString());
+        //    //                                                 base..Toggle();
+        //    //                                             }
+        //    //                                            )),
+
+
+        //};
     }
+}
 }
