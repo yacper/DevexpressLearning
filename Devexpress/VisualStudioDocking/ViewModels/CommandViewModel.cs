feff9f22bb4647505e2d1ba12914c2461abaa98c
yacper <yacper@gmail.com>
2023-04-10 22:46:26 +0800

    .

--- Devexpress/VisualStudioDocking/ViewModels/CommandViewModel.cs
+++ Devexpress/VisualStudioDocking/ViewModels/CommandViewModel.cs
@@ -14,23 +14,31 @@ namespace VisualStudioDocking.ViewModels;
 
 public class CommandViewModel : ViewModel
 {
-    public CommandViewModel() { }
+    public ICommand               Command     { get; private set; }
+    public List<CommandViewModel> Commands    { get; set; }
+    public BarItemDisplayMode     DisplayMode { get; set; }
+    public bool                   IsComboBox  { get; set; }
+    public bool                   IsEnabled   { get; set; }
+    public bool                   IsSeparator { get; set; }
+    public bool                   IsSubItem   { get; set; }
+    public KeyGesture             KeyGesture  { get; set; }
+    public WorkspaceViewModel     Owner       { get; private set; }
 
+
+#region c&d
+    public CommandViewModel() { }
     public CommandViewModel(string displayName, List<CommandViewModel> subCommands)
         : this(displayName, null, null, subCommands)
     {
     }
-
     public CommandViewModel(string displayName, ICommand command = null)
         : this(displayName, null, command, null)
     {
     }
-
     public CommandViewModel(WorkspaceViewModel owner, ICommand command)
         : this(string.Empty, owner, command)
     {
     }
-
     private CommandViewModel(string displayName, WorkspaceViewModel owner = null, ICommand command = null, List<CommandViewModel> subCommands = null)
     {
         IsEnabled = true;
@@ -45,14 +53,6 @@ public class CommandViewModel : ViewModel
         Command  = command;
         Commands = subCommands;
     }
+#endregion
 
-    public ICommand               Command     { get; private set; }
-    public List<CommandViewModel> Commands    { get; set; }
-    public BarItemDisplayMode     DisplayMode { get; set; }
-    public bool                   IsComboBox  { get; set; }
-    public bool                   IsEnabled   { get; set; }
-    public bool                   IsSeparator { get; set; }
-    public bool                   IsSubItem   { get; set; }
-    public KeyGesture             KeyGesture  { get; set; }
-    public WorkspaceViewModel     Owner       { get; private set; }
-}
+  }
